bae841400fcbeb399d343ff198eb3d2f302c839e
Jou Okuyama <ojoxux@gmail.com>
2025-07-10 13:19:10 +0900

    feat: AuthViewModelのユニットテスト実装完了

--- lib/core/utils/error_utils.dart
+++ lib/core/utils/error_utils.dart
@@ -2,17 +2,17 @@ import '../error/failures.dart';
 
 /// Failureからエラーメッセージを取得するヘルパー関数
 String getErrorMessage(Failure failure) {
-  switch (failure.runtimeType) {
-    case ServerFailure _:
-      return (failure as ServerFailure).message;
-    case NetworkFailure _:
-      return (failure as NetworkFailure).message;
-    case AuthFailure _:
-      return (failure as AuthFailure).message;
-    case ValidationFailure _:
-      return (failure as ValidationFailure).message;
-    case CacheFailure _:
-      return (failure as CacheFailure).message;
+  switch (failure) {
+    case ServerFailure():
+      return failure.message;
+    case NetworkFailure():
+      return failure.message;
+    case AuthFailure():
+      return failure.message;
+    case ValidationFailure():
+      return failure.message;
+    case CacheFailure():
+      return failure.message;
     default:
       return '予期せぬエラーが発生しました';
   }
